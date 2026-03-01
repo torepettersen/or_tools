72bfb334e54777c304e0ccf4d20de8355b06d9be
Tore Pettersen <toreskog@live.com>
2026-03-01 20:05:54 +0100

    Initial commit

---
+++ lib/or_tools.ex
@@ -0,0 +1,18 @@
+defmodule OrTools do
+  @moduledoc """
+  Documentation for `OrTools`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OrTools.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/or_tools_test.exs
@@ -0,0 +1,8 @@
+defmodule OrToolsTest do
+  use ExUnit.Case
+  doctest OrTools
+
+  test "greets the world" do
+    assert OrTools.hello() == :world
+  end
+end
